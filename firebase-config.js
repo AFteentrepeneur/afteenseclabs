2c38b266592ff7d501eca24d9e6df5eadf73a3f5
AFteentrepeneur <abdulafeezolawaleolaleye@gmail.com>
2026-03-17 14:39:32 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,11 @@
-// firebase-config.js — This file is injected safely by Vercel
+
+
+// firebase-config.js — Secure config injected by Vercel
 window.firebaseConfig = {
-  apiKey: "AIzaSyBWgzPiUi5RU9iL4ZfRfmnFvHmyUEHjggQ",           // ← Replace with your real apiKey
+  apiKey: "AIzaSyBWgzPiUi5RU9iL4ZfRfmnFvHmyUEHjggQ",                    // ← Replace with your real apiKey
   authDomain: "afteenseclabs-aabd0.firebaseapp.com",
   projectId: "afteenseclabs-aabd0",
   storageBucket: "afteenseclabs-aabd0.firebasestorage.app",
-  messagingSenderId: "45301896148",
-  appId: "1:45301896148:web:bff416b9ea807a9440bc41"
+  messagingSenderId:  "45301896148",
+  appId:"1:45301896148:web:bff416b9ea807a9440bc41"
 };
